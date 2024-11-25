f008b956e5316699f20fd9137647833a6c922ff4
NastasKorneli <korneli.nastas@gmail.com>
2024-11-25 14:30:20 +0100

    done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,72 +1,12 @@
+/* ===================================================================
+ * Luther 1.0.0 - Main JS
+ *
+ * ------------------------------------------------------------------- */
 
 (function (html) {
   "use strict";
 
   html.className = html.className.replace(/\bno-js\b/g, "") + " js ";
-
-  /* Animations
-   * -------------------------------------------------- */
-  const tl = anime
-    .timeline({
-      easing: "easeInOutCubic",
-      duration: 800,
-      autoplay: false,
-    })
-    .add({
-      targets: "#loader",
-      opacity: 0,
-      duration: 1000,
-      begin: function (anim) {
-        window.scrollTo(0, 0);
-      },
-    })
-    .add({
-      targets: "#preloader",
-      opacity: 0,
-      complete: function (anim) {
-        document.querySelector("#preloader").style.visibility = "hidden";
-        document.querySelector("#preloader").style.display = "none";
-      },
-    })
-    .add(
-      {
-        targets: ".s-header",
-        translateY: [-100, 0],
-        opacity: [0, 1],
-      },
-      "-=200"
-    )
-    .add({
-      targets: [".s-intro .text-pretitle", ".s-intro .text-huge-title"],
-      translateX: [100, 0],
-      opacity: [0, 1],
-      delay: anime.stagger(400),
-    })
-    .add({
-      targets: ".circles span",
-      keyframes: [
-        { opacity: [0, 0.3] },
-        {
-          opacity: [0.3, 0.1],
-          delay: anime.stagger(100, { direction: "reverse" }),
-        },
-      ],
-      delay: anime.stagger(100, { direction: "reverse" }),
-    })
-    .add({
-      targets: ".intro-social li",
-      translateX: [-50, 0],
-      opacity: [0, 1],
-      delay: anime.stagger(100, { direction: "reverse" }),
-    })
-    .add(
-      {
-        targets: ".intro-scrolldown",
-        translateY: [100, 0],
-        opacity: [0, 1],
-      },
-      "-=800"
-    );
 
   /* Preloader
    * -------------------------------------------------- */
@@ -78,17 +18,8 @@
       document.querySelector("html").classList.remove("ss-preload");
       document.querySelector("html").classList.add("ss-loaded");
 
-      document.querySelectorAll(".ss-animated").forEach(function (item) {
-        item.classList.remove("ss-animated");
-      });
-
-      tl.play();
+      preloader.style.display = "none";
     });
-
-    // force page scroll position to top at page refresh
-    // window.addEventListener('beforeunload' , function () {
-    //     // window.scrollTo(0, 0);
-    // });
   }; // end ssPreloader
 
   /* Mobile Menu
@@ -108,7 +39,6 @@
 
     mainNavWrap.querySelectorAll(".main-nav a").forEach(function (link) {
       link.addEventListener("click", function (event) {
-        // at 800px and below
         if (window.matchMedia("(max-width: 800px)").matches) {
           toggleButton.classList.toggle("is-clicked");
           siteBody.classList.toggle("menu-is-open");
@@ -117,7 +47,6 @@
     });
 
     window.addEventListener("resize", function () {
-      // above 800px
       if (window.matchMedia("(min-width: 801px)").matches) {
         if (siteBody.classList.contains("menu-is-open"))
           siteBody.classList.remove("menu-is-open");
@@ -127,266 +56,60 @@
     });
   }; // end ssMobileMenu
 
-  /* Highlight active menu link on pagescroll
-   * ------------------------------------------------------ */
-  const ssScrollSpy = function () {
-    const sections = document.querySelectorAll(".target-section");
-
-    // Add an event listener listening for scroll
-    window.addEventListener("scroll", navHighlight);
-
-    function navHighlight() {
-        // Get current scroll position
-        let scrollY = window.pageYOffset;
-
-        // Loop through sections to get height, top, and ID values for each
-        sections.forEach(function (current) {
-            const sectionHeight = current.offsetHeight;
-            const sectionTop = current.offsetTop - 50;
-            const sectionId = current.getAttribute("id");
-
-            /* If our current scroll position enters the space where current section
-             * on screen is, add .current class to parent element(li) of the
-             * corresponding navigation link, else remove it.
-             */
-            const navLink = document.querySelector(".main-nav a[href*=" + sectionId + "]");
-
-            // Проверка, найден ли navLink, прежде чем пытаться получить его parentNode
-            if (navLink) {
-                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                    navLink.parentNode.classList.add("current");
-                } else {
-                    navLink.parentNode.classList.remove("current");
-                }
-            } else {
-                console.warn("Навигационная ссылка не найдена для секции:", sectionId);
-            }
-        });
-    }
-}; // end ssScrollSpy
-  const ssViewAnimate = function () {
-    const blocks = document.querySelectorAll("[data-animate-block]");
-
-    window.addEventListener("scroll", viewportAnimation);
-
-    function viewportAnimation() {
-      let scrollY = window.pageYOffset;
-
-      blocks.forEach(function (current) {
-        const viewportHeight = window.innerHeight;
-        const triggerTop =
-          current.offsetTop + viewportHeight * 0.2 - viewportHeight;
-        const blockHeight = current.offsetHeight;
-        const blockSpace = triggerTop + blockHeight;
-        const inView = scrollY > triggerTop && scrollY <= blockSpace;
-        const isAnimated = current.classList.contains("ss-animated");
-
-        if (inView && !isAnimated) {
-          anime({
-            targets: current.querySelectorAll("[data-animate-el]"),
-            opacity: [0, 1],
-            translateY: [100, 0],
-            delay: anime.stagger(400, { start: 200 }),
-            duration: 800,
-            easing: "easeInOutCubic",
-            begin: function (anim) {
-              current.classList.add("ss-animated");
-            },
-          });
-        }
-      });
-    }
-  }; // end ssViewAnimate
-
-  /* Swiper
-   * ------------------------------------------------------ */
-  const ssSwiper = function () {
-    const mySwiper = new Swiper(".swiper-container", {
-      slidesPerView: 1,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      breakpoints: {
-        // when window width is > 400px
-        401: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        // when window width is > 800px
-        801: {
-          slidesPerView: 2,
-          spaceBetween: 32,
-        },
-        // when window width is > 1200px
-        1201: {
-          slidesPerView: 2,
-          spaceBetween: 80,
-        },
-      },
-    });
-  }; // end ssSwiper
-
-  /* Lightbox
-   * ------------------------------------------------------ */
-  const ssLightbox = function () {
-    const folioLinks = document.querySelectorAll(".folio-list__item-link");
-    const modals = [];
-
-    folioLinks.forEach(function (link) {
-      let modalbox = link.getAttribute("href");
-      let instance = basicLightbox.create(document.querySelector(modalbox), {
-        onShow: function (instance) {
-          //detect Escape key press
-          document.addEventListener("keydown", function (event) {
-            event = event || window.event;
-            if (event.keyCode === 27) {
-              instance.close();
-            }
-          });
-        },
-      });
-      modals.push(instance);
-    });
-
-    folioLinks.forEach(function (link, index) {
-      link.addEventListener("click", function (event) {
-        event.preventDefault();
-        modals[index].show();
-      });
-    });
-  }; // end ssLightbox
-
-  /* Alert boxes
-   * ------------------------------------------------------ */
-  const ssAlertBoxes = function () {
-    const boxes = document.querySelectorAll(".alert-box");
-
-    boxes.forEach(function (box) {
-      box.addEventListener("click", function (event) {
-        if (event.target.matches(".alert-box__close")) {
-          event.stopPropagation();
-          event.target.parentElement.classList.add("hideit");
-
-          setTimeout(function () {
-            box.style.display = "none";
-          }, 500);
-        }
-      });
-    });
-  }; // end ssAlertBoxes
-
-  /* Smoothscroll
-   * ------------------------------------------------------ */
-  const ssMoveTo = function () {
-    const easeFunctions = {
-      easeInQuad: function (t, b, c, d) {
-        t /= d;
-        return c * t * t + b;
-      },
-      easeOutQuad: function (t, b, c, d) {
-        t /= d;
-        return -c * t * (t - 2) + b;
-      },
-      easeInOutQuad: function (t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return (c / 2) * t * t + b;
-        t--;
-        return (-c / 2) * (t * (t - 2) - 1) + b;
-      },
-      easeInOutCubic: function (t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return (c / 2) * t * t * t + b;
-        t -= 2;
-        return (c / 2) * (t * t * t + 2) + b;
-      },
-    };
-
-    const triggers = document.querySelectorAll(".smoothscroll");
-
-    const moveTo = new MoveTo(
-      {
-        tolerance: 0,
-        duration: 1200,
-        easing: "easeInOutCubic",
-        container: window,
-      },
-      easeFunctions
-    );
-
-    triggers.forEach(function (trigger) {
-      moveTo.registerTrigger(trigger);
-    });
-  }; // end ssMoveTo
-
   /* Initialize
    * ------------------------------------------------------ */
   (function ssInit() {
     ssPreloader();
     ssMobileMenu();
-    ssScrollSpy();
-    ssViewAnimate();
-    ssSwiper();
-    ssLightbox();
-    ssAlertBoxes();
-    ssMoveTo();
   })();
 })(document.documentElement);
 
+document.addEventListener("DOMContentLoaded", function () {
+  const currentPath =
+    window.location.pathname.split("/").pop() || "index.html";
 
-document.addEventListener("DOMContentLoaded", function() {
-  const currentPath = window.location.pathname.split("/").pop() || "index.html";
+  document.querySelectorAll(".main-nav a").forEach((link) => {
+    const href = link.getAttribute("href");
 
-  document.querySelectorAll('.main-nav a').forEach(link => {
-      const href = link.getAttribute('href');
-      
-      if (href === currentPath) {
-          link.classList.add('current');
-          link.style.backgroundColor = "#2c2c2d";  // Устанавливаем фон
-      } else {
-          link.classList.remove('current');
-          link.style.backgroundColor = "";         // Сбрасываем фон для остальных
-      }
+    if (href === currentPath) {
+      link.classList.add("current");
+      link.style.backgroundColor = "#d65369";
+    } else {
+      link.classList.remove("current");
+      link.style.backgroundColor = "";
+    }
   });
 });
 
 function toggleAnswer(questionElement) {
   const answer = questionElement.nextElementSibling;
-
-  // Toggle 'active' class on answer
   answer.classList.toggle("active");
-
-  // Toggle icon rotation for the chevron
   const icon = questionElement.querySelector("i");
   icon.classList.toggle("rotate");
 }
 
-    function addHover(element) {
-        if (!element.classList.contains('active')) {
-            element.style.backgroundColor = '#d65369'; // Добавляем фон при hover, если нет класса active
-        }
-    }
-    
-    function removeHover(element) {
-        if (!element.classList.contains('active')) {
-            element.style.backgroundColor = ''; // Убираем hover-цвет, возвращая стиль к исходному
-        }
-    }
-    
-    function toggleActive(element) {
-        if (element.classList.contains('active')) {
-            element.classList.remove('active');
-            element.style.backgroundColor = ''; // Возвращаем цвет при отключении активного состояния
-        } else {
-            document.querySelectorAll('.tag.btn.btn--small').forEach(btn => {
-                btn.classList.remove('active');
-                btn.style.backgroundColor = '';
-            });
-            element.classList.add('active');
-            element.style.backgroundColor = '#d65369'; // Активное состояние
-        }
-    }
+function addHover(element) {
+  if (!element.classList.contains("active")) {
+    element.style.backgroundColor = "#d65369";
+  }
+}
 
+function removeHover(element) {
+  if (!element.classList.contains("active")) {
+    element.style.backgroundColor = "";
+  }
+}
 
-  
-
+function toggleActive(element) {
+  if (element.classList.contains("active")) {
+    element.classList.remove("active");
+    element.style.backgroundColor = "";
+  } else {
+    document.querySelectorAll(".tag.btn.btn--small").forEach((btn) => {
+      btn.classList.remove("active");
+      btn.style.backgroundColor = "";
+    });
+    element.classList.add("active");
+    element.style.backgroundColor = "#d65369";
+  }
+}
